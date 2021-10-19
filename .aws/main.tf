e0978a7103da8ec13b52b4ce53c77f7f41eb4256
Matt Whitney <mattjwhitney@gmail.com>
2021-10-19 07:48:36 -0400

    Update

--- .aws/main.tf
+++ .aws/main.tf
@@ -1,8 +1,17 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
 terraform {
   backend "s3" {
     bucket = "mjw.terraform"
     key    = "mjw.sh/terraform.state"
     region = "us-east-1"
   }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.63.0"
+    }
+  }
 }
-
